7fcf17f941e69c27f19337036c1ca540c6d0e687
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-09-20 22:13:31 +0600

    country info show

--- src/App.js
+++ src/App.js
@@ -21,6 +21,20 @@ function LoadCountries() {
     <div>
       <h1>Visiting Countries</h1>
       <h3>Available: {countries.length}</h3>
+      {
+        countries.map((country) => 
+         <Country name={country.name.common} population={country.population} key={country.name.common.toString()}></Country>
+        )
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: { props.name}</h2>
+      <h4>Population: { props.population}</h4>
     </div>
   )
 }
